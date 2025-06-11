d5560dfbc4bd880a6e9a053f15708319d048de79
Diego Rivera <dirivera@espol.edu.ec>
2025-06-11 10:09:18 -0500

    First commit of the project, creating POES functoin using PR-9 code

--- test1.py
+++ test1.py
@@ -5,3 +5,4 @@ st.title('My first app using Python')
 st.subheader('App for Petroleum Engineering')
 
 st.write('This app is going to help you to understand the digitalization process of oil and gas industry.')
+
